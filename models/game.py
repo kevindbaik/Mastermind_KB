a93712cc41bcfd6bb62aaa2e1882b2e8981bf4a0
kevin <kevindbaik@gmail.com>
2024-04-23 19:10:42 -0700

    added difficulty getter/setter + validations, added difficulty effect

--- models/game.py
+++ models/game.py
@@ -11,9 +11,23 @@ class Game:
   # ------- methods ----------
 
 
+  # --------- getters/setters ---------
+  @property
+  def difficulty(self):
+    return self._difficulty
+
+  @difficulty.setter
+  def difficulty(self, user_input):
+    if not isinstance(user_input, int):
+      raise ValueError("Input must be a number")
+    if user_input not in [1, 2, 3]:
+      raise ValueError("Input must be 1,2,or 3")
+    self._difficulty = user_input
+
   # ------- private methods ----------
   def _generate_answer(self):
-    url = f"https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new"
+    range = self._generate_max_range()
+    url = f"https://www.random.org/integers/?num={range}&min=0&max=7&col=1&base=10&format=plain&rnd=new"
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -23,3 +37,12 @@ class Game:
     else:
       print("Failed to retrieve data...")
       return "9999"
+
+  def _generate_max_range(self):
+    match self.difficulty:
+      case 1:
+        return 4
+      case 2:
+        return 5
+      case 3:
+        return 6
--- tests/test_game.py
+++ tests/test_game.py
@@ -2,11 +2,39 @@ import unittest
 from models.game import Game
 
 class TestGame(unittest.TestCase):
-  # making sure API returns
-  def test_random_api(self):
-    game = Game(1)
-    self.assertEqual(len(game.answer), 4)
-    self.assertNotEqual(game.answer[3], 9)
+
+  # difficulty validation
+  def test_difficulty_valid(self):
+    user_choice1 = 1
+    game1 = Game(user_choice1)
+    self.assertEqual(game1.difficulty, 1)
+
+    user_choice2 = 2
+    game2 = Game(user_choice2)
+    self.assertEqual(game2.difficulty, 2)
+
+    user_choice3 = 3
+    game3 = Game(user_choice3)
+    self.assertEqual(game3.difficulty, 3)
+
+  def test_difficulty_invalid_str(self):
+    user_choice = "easy"
+    self.assertRaisesRegex(ValueError, "Input must be a number", lambda: Game(user_choice))
+
+  def test_difficulty_invalid_range(self):
+    user_choice = 5
+    self.assertRaisesRegex(ValueError, "Input must be 1,2,or 3", lambda: Game(user_choice))
+
+  # api returns valid answer
+  def test_api_random(self):
+    easy_game = Game(1)
+    self.assertEqual(len(easy_game.answer), 4)
+
+    med_game = Game(2)
+    self.assertEqual(len(med_game.answer), 5)
+
+    hard_game = Game(3)
+    self.assertEqual(len(hard_game.answer), 6)
 
 if __name__ == '__main__':
   unittest.main()
